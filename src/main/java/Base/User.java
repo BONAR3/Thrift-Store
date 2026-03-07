77f0fedb43078a4f806b97a06da864e7be7cd42f
Abdulroqib Adeleke <162007856+Abdulroqib-Adeleke@users.noreply.github.com>
2026-03-07 14:04:28 +0100

    Added fields, constructor and getter methods to the User class

--- src/main/java/Base/User.java
+++ src/main/java/Base/User.java
@@ -1,5 +1,33 @@
 package Base;
 
-abstract public class User {
+public abstract class User {
 
+    private final String name;
+    private final String email;
+    private final int phoneNumber;
+    private int age;
+
+
+    public User(int age, int phoneNumber, String email, String name) {
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
