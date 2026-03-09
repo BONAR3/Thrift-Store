a20a88597b9eadd96bfd69c8a08d915b0966a076
BONAR3 <aruaobinnaty@gmail.com>
2026-03-09 22:32:53 +0100

    Commiting changes to admin and seller class

--- src/main/java/Entity/Admin.java
+++ src/main/java/Entity/Admin.java
@@ -1,4 +1,15 @@
 package Entity;
 
-public class Admin {
+import Base.User;
+
+public class Admin extends User {
+
+    private String password;
+    private int adminId;
+
+    public Admin(String name, String email, int phoneNumber, int age, String password, int adminId) {
+        super(name, email, phoneNumber, age);
+        this.password = password;
+        this.adminId = adminId;
+    }
 }
--- src/main/java/Entity/Seller.java
+++ src/main/java/Entity/Seller.java
@@ -1,4 +1,15 @@
 package Entity;
 
-public class Seller {
+import Base.User;
+
+public class Seller extends User {
+
+    private int sellerID;
+    private String password;
+
+    public Seller(String name, String email, int phoneNumber, int age, int sellerID, String password) {
+        super(name, email, phoneNumber, age);
+        this.sellerID = sellerID;
+        this.password = password;
+    }
 }
